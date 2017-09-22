08897a13429e754a29d460e12403a713e74bee29
Pavel Macík <pmacik@redhat.com>
2017-09-22 14:49:56 +0200

    travis

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 #set -e
 
 #export PING_SLEEP=30s
-export MAVEN_OPTS="-Xmx326m -Dmaven.test.failure.ignore=true";
+export MAVEN_OPTS="-Xmx326m";
 
 # Set up a repeating loop to send some output to Travis.
 #bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
@@ -11,13 +11,18 @@ export MAVEN_OPTS="-Xmx326m -Dmaven.test.failure.ignore=true";
 
 # My build is using maven, but you could build anything with this, E.g.
 
-mvn package assembly:single -DskipTests
-cp -vf ./perfcake/target/perfcake-$TRAVIS_TAG-bin.zip .
-cp -vf ./perfcake/target/perfcake-$TRAVIS_TAG-bin.tar.gz .
-cp -vf ./perfcake/target/perfcake-$TRAVIS_TAG-bin.tar.bz2 .
-cp -vf ./perfcake-agent/target/perfcake-agent-$TRAVIS_TAG-bin-agent.zip .
-cp -vf ./perfcake-agent/target/perfcake-agent-$TRAVIS_TAG-bin-agent.tar.gz .
-cp -vf ./perfcake-agent/target/perfcake-agent-$TRAVIS_TAG-bin-agent.tar.bz2 .
+mvn install -DskipTests
+mvn -pl perfcake package assembly:single -DskipTests
+mvn -pl perfcake-agent package assembly:single -DskipTests
+
+export PERFCAKE_VERSION=`echo $TRAVIS_TAG | cut -d'v' -f 2`
+
+cp -vf ./perfcake/target/perfcake-$PERFCAKE_VERSION-bin.zip .
+cp -vf ./perfcake/target/perfcake-$PERFCAKE_VERSION-bin.tar.gz .
+cp -vf ./perfcake/target/perfcake-$PERFCAKE_VERSION-bin.tar.bz2 .
+cp -vf ./perfcake-agent/target/perfcake-agent-$PERFCAKE_VERSION-bin-agent.zip .
+cp -vf ./perfcake-agent/target/perfcake-agent-$PERFCAKE_VERSION-bin-agent.tar.gz .
+cp -vf ./perfcake-agent/target/perfcake-agent-$PERFCAKE_VERSION-bin-agent.tar.bz2 .
 
 #maven_exit=$?
 #if [[ $maven_exit -ne 0 ]];
