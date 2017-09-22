d920e559bf8b9410a97b7ba7a5c1bd89823fc150
Pavel Macík <pmacik@redhat.com>
2017-09-22 13:40:25 +0200

    travis

--- travis.sh
+++ travis.sh
@@ -2,22 +2,22 @@
 # Abort on Error
 #set -e
 
-export PING_SLEEP=30s
+#export PING_SLEEP=30s
 export MAVEN_OPTS="-Xmx326m -Dmaven.test.failure.ignore=true";
 
 # Set up a repeating loop to send some output to Travis.
-bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
-export PING_LOOP_PID=$!
+#bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+#export PING_LOOP_PID=$!
 
 # My build is using maven, but you could build anything with this, E.g.
-mvn -l maven.log package assembly:single -DskipTests
+mvn package assembly:single -DskipTests
 
-maven_exit=$?
-if [[ $maven_exit -ne 0 ]];
-then
-	tail -c 2M maven.log;
-fi
+#maven_exit=$?
+#if [[ $maven_exit -ne 0 ]];
+#then
+#	tail -c 2M maven.log;
+#fi
 
 # nicely terminate the ping output loop
-kill $PING_LOOP_PID
-exit $maven_exit
+#kill $PING_LOOP_PID
+#exit $maven_exit
